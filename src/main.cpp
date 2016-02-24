b072e06655c5afb9078995c7252d627ad896faa2
Maxim <netut.maksim@gmail.com>
2016-02-24 00:42:13 -0800

    libev is stupid lib!

--- src/main.cpp
+++ src/main.cpp
@@ -25,15 +25,15 @@
 using namespace std;
 
 //handlers by socket and own for each thread
-thread_local map<int,HttpHandler*> m_serverMap;
+//thread_local map<int,HttpHandler*> m_serverMap;
 
 const char* dir=NULL;
 
 //for simplicity don't get from OS real number of cores
-static const int numberKernel=2;
+//static const int numberKernel=2;
 
 //one loop per one work thread
-static struct ev_loop* loops[numberKernel];
+//static struct ev_loop* loops[numberKernel];
 
 //for manage work thread
 vector<thread> threads;
@@ -42,79 +42,89 @@ struct ev_loop* main_loop=NULL;
 
 std::ofstream out;
 
-extern void read_data(struct ev_loop *loop, struct ev_io *watcher, int revents);
+//extern void read_data(struct ev_loop *loop, struct ev_io *watcher, int revents);
 
 
-static void idle_cb (EV_P_ ev_idle *w, int revents)
+//static void idle_cb (EV_P_ ev_idle *w, int revents)
+//{
+//    //TRACE() << "idle!"  << endl;
+//}
+
+
+//void threadFunc(int i)
+//{
+
+
+//    TRACE() << "thread " << std::this_thread::get_id() << "loop = " << i <<  endl;
+
+//    ev_idle idle; // actual processing watcher
+//    ev_io io;     // actual event watcher
+
+//    // initialisation
+//    ev_idle_init (&idle, idle_cb);
+//    ev_idle_start (loops[i],&idle);
+
+
+//    while(1)
+//    {
+//        ev_loop(loops[i], 0);
+//        TRACE() << "restart loop i =" << i << endl;
+//    }
+
+//    TRACE() << "after " << i << endl;
+//}
+
+
+///* Accept client requests */
+//void read_data(struct ev_loop *loop, struct ev_io *watcher, int revents)
+//{
+//    TRACE() << "thread " << std::this_thread::get_id() << endl;
+
+//    if(EV_ERROR & revents)
+//    {
+//        TRACE() << "got invalid event";
+//        return;
+//    }
+
+//    if(!(EV_READ & revents))
+//    {
+//        return;
+//    }
+
+//    HttpHandler* handler = NULL;
+
+//    //find handler...
+//    if (m_serverMap.find(watcher->fd) != m_serverMap.end())
+//        handler = m_serverMap[watcher->fd];
+//    else
+//    {
+//        handler = new HttpHandler(watcher->fd,dir);
+//        m_serverMap[watcher->fd] = handler;
+//    }
+
+//    //if any error or not keep-alive - close connection and unregister watcher
+//    if (handler->processing() <=0)
+//    {
+//        m_serverMap.erase(watcher->fd);
+//        delete handler;
+
+//        shutdown(watcher->fd,SHUT_RDWR);
+//        close(watcher->fd);
+//        ev_io_stop(loop,watcher);
+
+//    }
+//}
+
+void threadFunc(int socket,const char* dir)
 {
-    //TRACE() << "idle!"  << endl;
+    HttpHandler handler(socket,dir);
+
+    handler.processing();
+
+    shutdown(socket,SHUT_RDWR);
+    close(socket);
+
 }
-
-
-void threadFunc(int i)
-{    
-
-
-    TRACE() << "thread " << std::this_thread::get_id() << "loop = " << i <<  endl;
-
-    ev_idle idle; // actual processing watcher
-    ev_io io;     // actual event watcher
-
-    // initialisation
-    ev_idle_init (&idle, idle_cb);
-    ev_idle_start (loops[i],&idle);
-
-
-    while(1)
-    {
-        ev_loop(loops[i], 0);
-        TRACE() << "restart loop i =" << i << endl;
-    }
-
-    TRACE() << "after " << i << endl;
-}
-
-
-/* Accept client requests */
-void read_data(struct ev_loop *loop, struct ev_io *watcher, int revents)
-{
-    TRACE() << "thread " << std::this_thread::get_id() << endl;
-
-    if(EV_ERROR & revents)
-    {
-        TRACE() << "got invalid event";
-        return;
-    }
-
-    if(!(EV_READ & revents))
-    {
-        return;
-    }
-
-    HttpHandler* handler = NULL;
-
-    //find handler...
-    if (m_serverMap.find(watcher->fd) != m_serverMap.end())
-        handler = m_serverMap[watcher->fd];
-    else
-    {
-        handler = new HttpHandler(watcher->fd,dir);
-        m_serverMap[watcher->fd] = handler;
-    }
-
-    //if any error or not keep-alive - close connection and unregister watcher
-    if (handler->processing() <=0)
-    {
-        m_serverMap.erase(watcher->fd);
-        delete handler;
-
-        shutdown(watcher->fd,SHUT_RDWR);
-        close(watcher->fd);
-        ev_io_stop(loop,watcher);
-
-    }
-}
-
 
 /* Accept client requests */
 void accept_connection(struct ev_loop *loop, struct ev_io *watcher, int revents)
@@ -146,56 +156,59 @@ void accept_connection(struct ev_loop *loop, struct ev_io *watcher, int revents)
 
     TRACE() << "Successfully connected with client.\n";
 
-    bool needStartThread=false;
+    //bool needStartThread=false;
 
     //loop yet doesn't exist
-    if (!loops[threadForConnect])
-    {
-        loops[threadForConnect] = ev_loop_new(EVBACKEND_EPOLL);
-        needStartThread=true;
-    }
+//    if (!loops[threadForConnect])
+//    {
+//        loops[threadForConnect] = ev_loop_new(EVBACKEND_EPOLL);
+//        needStartThread=true;
+//    }
 
-    TRACE() << "ACCEPT!!!! 3 threadForConnect =" << threadForConnect  << endl;
+//    TRACE() << "ACCEPT!!!! 3 threadForConnect =" << threadForConnect  << endl;
 
-    // Initialize and start watcher to read client requests
-    ev_io_init(w_client, read_data, client_sd, EV_READ);
-    ev_io_start(loops[threadForConnect], w_client);
+//    // Initialize and start watcher to read client requests
+//    ev_io_init(w_client, read_data, client_sd, EV_READ);
+//    ev_io_start(loops[threadForConnect], w_client);
 
-    if (needStartThread)
-    {
-        //create and start thread only after register watcher because without watcher even loop will finished
-        threads.push_back(thread(threadFunc,threadForConnect));
-    }
+//    if (needStartThread)
+//    {
+//        //create and start thread only after register watcher because without watcher even loop will finished
+//        threads.push_back(thread(threadFunc,threadForConnect));
+//    }
 
-    //next incoming request by handling next thread in ring
-    threadForConnect = (threadForConnect + 1) % numberKernel;
+//    //next incoming request by handling next thread in ring
+//    threadForConnect = (threadForConnect + 1) % numberKernel;
+
+    threads.push_back(thread(threadFunc,client_sd,dir));
+
 }
 
-//for gracefully quit
-void final_handler(int signum)
-{
-    TRACE() << "!!!" << endl;
-    int i = 0;
+////for gracefully quit
+//void final_handler(int signum)
+//{
+//    TRACE() << "!!!" << endl;
+//    int i = 0;
 
-    while (i < numberKernel)
-    {
-        if (loops[i])
-        {
-            ev_break(loops[i]);
-        }
-        i++;
-    }
+//    while (i < numberKernel)
+//    {
+//        if (loops[i])
+//        {
+//            ev_break(loops[i]);
+//        }
+//        i++;
+//    }
 
-    if (main_loop)
-        ev_break(main_loop);
+//    if (main_loop)
+//        ev_break(main_loop);
 
-    for(thread& thr:threads)
-        thr.join();
+//    for(thread& thr:threads)
+//        thr.join();
 
-    TRACE() << "bye..." << endl;
+//    TRACE() << "bye..." << endl;
 
-    exit(EXIT_SUCCESS);
-}
+//    exit(EXIT_SUCCESS);
+//}
 
 void daemonize()
 {
@@ -309,8 +322,8 @@ int main(int argc, char *argv[])
     out.open("/tmp/log.txt");
 #endif
 
-    signal(SIGINT, final_handler);
-    signal(SIGTERM, final_handler);
+    //signal(SIGINT, final_handler);
+    //signal(SIGTERM, final_handler);
 
     int sock = 0;
 
